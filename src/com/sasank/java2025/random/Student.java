5d129a8b0b9d27b6677f2db6a1c975a0fef5801f
Sasank Reddyi <sasankreddy60@gmail.com>
2026-01-07 05:22:09 -0800

    Add grade calculation and refactor input handling

--- src/com/sasank/java2025/random/Student.java
+++ src/com/sasank/java2025/random/Student.java
@@ -6,30 +6,6 @@ public class Student {
 	private String name;
 	private int maths,physics,chemistry;
 	
-//	public Student(int id, String name, int maths, int physics, int chemistry) {
-//		this.id = id;
-//		this.name = name;
-//		this.chemistry = chemistry;
-//		this.maths = maths;
-//		this.physics = physics;
-//	}
-	public Student() {
-	}
-	public void readInput() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter id : ");
-		int id = sc.nextInt();
-		sc.nextLine();
-		System.out.println("Enter name : ");
-		String name = sc.nextLine();
-		System.out.println("Enter maths : ");
-		int maths = sc.nextInt();
-		System.out.println("Enter physics : ");
-		int physics = sc.nextInt();
-		System.out.println("Enter chemistry : ");
-		int chemistry = sc.nextInt();
-	}
-	
 	public double calculateTotal() {
 		
 		return maths+physics+chemistry;
@@ -40,34 +16,47 @@ public class Student {
 		return calculateTotal()/3.0;
 	}
 	
+	public void  readInput(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter id : ");
+		this.id = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter name : ");
+		this.name = sc.nextLine();
+		System.out.println("Enter maths : ");
+		this.maths = sc.nextInt();
+		System.out.println("Enter physics : ");
+		this.physics = sc.nextInt();
+		System.out.println("Enter chemistry : ");
+		this.chemistry = sc.nextInt();
+	}
+	public char calculateGrade() {
+		char grade = 0;
+		if(calculateAverage() >= 90) {
+			grade = 'A';
+		}else if(calculateAverage() < 90 || calculateAverage() >= 70) {
+			grade = 'B';
+		}else if(calculateAverage() < 70) {
+			grade = 'F';
+		}
+		return grade;
+	}
+	
 	public void displayResult() {
 		
-		System.out.println("id = " + id);
+		readInput();
+		System.out.println("id = " + this.id);
 		System.out.println("name = " + name);
 		System.out.println("chemistry = " + chemistry);
 		System.out.println("maths = " + maths);
 		System.out.println("physics = " + physics);
 		System.out.println("Total = " + calculateTotal());
 		System.out.println("Average = " + calculateAverage());
+		System.out.println("Average = " + calculateGrade());
 	}
 
 	public static void main(String[] args) {
-		
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter id : ");
-//		int id = sc.nextInt();
-//		sc.nextLine();
-//		System.out.println("Enter name : ");
-//		String name = sc.nextLine();
-//		System.out.println("Enter maths : ");
-//		int maths = sc.nextInt();
-//		System.out.println("Enter physics : ");
-//		int physics = sc.nextInt();
-//		System.out.println("Enter chemistry : ");
-//		int chemistry = sc.nextInt();
-	
 		Student s = new Student();
-		s.readInput();
 		s.displayResult();
 	}
 
